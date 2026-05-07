f321a89aa343709f931f9aad8cf3bfbb83eb14b7
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-05-07 15:11:55 +0300

    Update FileHandler.h

--- include/FileHandler.h
+++ include/FileHandler.h
@@ -3,7 +3,12 @@
 #include <vector>
 class FileHandler{
 public:
-static std:: vector <unsigned char> readfile (const std:: string & filepath);
+static std:: vector <unsigned char> readfile (const std:: string & filepath); 
+
+
 static void writefile  (const std:: string & filepath, const std::vector<unsigned char>& data) ;
 
+
+
+static void encryptDecrypt(std::vector<unsigned char>& data, const std::string& key);
 };
